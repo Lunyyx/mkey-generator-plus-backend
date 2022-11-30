ba8ce4b3c92d7965a2e125ae3e79a676b9d5da72
Lunyx <thedarkmaster71@hotmail.com>
2022-11-30 23:17:15 +0100

    Better errors handling

--- index.js
+++ index.js
@@ -10,34 +10,31 @@ app.get('/', (req, res) => {
     inquiryNb = req.query.inquiryNumber
     device = req.query.device
 
-    if(month > 12 || month < 1)
-        res.send({error: 'The parameter \'m\' must between 1 and 12.'}) 
-
-    if(day > 31 || day < 1)
-        res.send({error: 'The parameter \'d\' must between 1 and 31.'}) 
-
-    if(inquiryNb.length != 10)
-        res.send({error: 'The parameter \'inquiryNb\' must have a length of 10.'})
-    
-    /* May change in the future */
-    if(device != 'CTR')
-        res.send({error: 'The parameter \'device\' needs to be set to \'CTR\'.'})
-
     res.set('Access-Control-Allow-Origin', '*');
 
-    let options = {
-        mode: 'json',
-        args: [`-m ${month}`, `-d ${day}`, inquiryNb, device]
-    }
-
-    PythonShell.run('mkey.py', options, function(err, results) {
-        if(err) {
-            console.log(err)
-            res.send({error: 'An error occured. Please check your arguments.'}) 
-        } else if (results) {
-            res.send({master_key: parseInt(results[0])})
+    if(month > 12 || month < 1) {
+        res.send({error: 'The month must between 1 and 12.'})
+    } else if(day > 31 || day < 1) {
+        res.send({error: 'The day must between 1 and 31.'}) 
+    } else if(inquiryNb.length != 10) {
+        res.send({error: 'The inquiry number must have a length of 10.'})
+    } else if(device != 'CTR') { 
+        res.send({error: 'The device needs to be set to CTR.'})
+    } else {
+        let options = {
+            mode: 'json',
+            args: [`-m ${month}`, `-d ${day}`, inquiryNb, device]
         }
-    })
+    
+        PythonShell.run('mkey.py', options, function(err, results) {
+            if(err) {
+                console.log(err)
+                res.send({error: 'An error has occured. Please check your arguments.'}) 
+            } else if (results) {
+                res.send({master_key: parseInt(results[0])})
+            }
+        })
+    }
 })
 
 app.listen(port, () => {
